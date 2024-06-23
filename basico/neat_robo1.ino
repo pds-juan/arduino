c7731ece4a1ccd137c7ef6aa2cb29084fdd90692
Juan <juan02pires@gmail.com>
2024-06-22 23:56:06 -0300

    ligar led

---
+++ basico/neat_robo1.ino
@@ -0,0 +1,16 @@
+int pinoLed = 13;
+
+void setup()
+{
+  pinMode(pinoLed, OUTPUT);
+  digitalWrite(pinoLed, HIGH);
+  
+  delay(2000); // milissegundos
+  
+  digitalWrite(pinoLed, LOW);
+}
+
+void loop()
+{
+  
+}
